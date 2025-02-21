cd537ef0be15195f635ab67b6a39cecdce8ca1eb
Cruciatoz <ademar.work@gmail.com>
2025-02-21 14:58:15 -0400

    Update Trunfo.c
    Mudança de vetor

--- Trunfo.c
+++ Trunfo.c
@@ -2,7 +2,7 @@
 
 int main(){
     char estado_1[20], Estado_2[50];
-    int codigo_da_carta_1,codigo_da_carta_2;
+    char codigo_da_carta_1[10],codigo_da_carta_2[10];
     char nome_da_cidade_1[50], nome_da_cidade_2[50];
     int populacao_1, populacao_2;
     float area_em_km²_1, area_em_km²_2;
@@ -19,7 +19,7 @@ int main(){
     scanf ("%s", &estado_1);
 
     printf("Codigo da carta: ");
-    scanf("%d", &codigo_da_carta_1);
+    scanf("%s", &codigo_da_carta_1);
 
     printf("=== Atencao: Digite a Cidade sem acento e sem espaços, substituindo o espaco por _ === \nNome da cidade: ");
     scanf("%s", &nome_da_cidade_1);
@@ -43,7 +43,7 @@ int main(){
     scanf ("%s", &Estado_2);
 
     printf("Codigo da carta: ");
-    scanf("%d", &codigo_da_carta_2);
+    scanf("%s", &codigo_da_carta_2);
 
     printf("=== Atencao: Digite a Cidade sem acento e sem espaços, substituindo o espaco por _ ===\nNome da cidade:  ");
     scanf("%s", &nome_da_cidade_2);
@@ -66,20 +66,20 @@ int main(){
 
     printf("\n --- Carta 1 ---: \n");
     printf("Estado: %s\n", estado_1);
-    printf("Codigo da carta: %d\n", codigo_da_carta_1);
+    printf("Codigo da carta: %s\n", codigo_da_carta_1);
     printf("Nome da cidade: %s\n", nome_da_cidade_1);
     printf("Populacao: %d\n", populacao_1);
-    printf("Pib: %.0f\n", Pib_1);
-    printf("Area em Km²: %.0f\n", area_em_km²_1);
+    printf("Pib: %.0f Bilhoes\n", Pib_1);
+    printf("Area em Km²: %.0f Km²\n", area_em_km²_1);
     printf("Pontos Turisticos: %d\n", pontos_turisticos_1);
 
     printf("\n --- Carta 2 ---: \n");
     printf("Estado: %s\n", Estado_2);
-    printf("Codigo da carta: %d\n", codigo_da_carta_2);
+    printf("Codigo da carta: %s\n", codigo_da_carta_2);
     printf("Nome da cidade: %s\n", nome_da_cidade_2);
     printf("Populacao: %d\n", populacao_2);
-    printf("Pib: %.0f\n", Pib_2);
-    printf("Area em Km²: %.0f\n", area_em_km²_2);
+    printf("Pib: %.0f Bilhoes\n", Pib_2);
+    printf("Area em Km²: %.0f Km²\n", area_em_km²_2);
     printf("Pontos Turisticos: %d\n", pontos_turisticos_2);
 
     return 0;
